308c319b3a5ace1ec034ebbe416f762394abfbf4
Udit <145645636+uditttttt@users.noreply.github.com>
2025-01-03 03:43:26 +0530

    Update 2559. Count Vowel Strings in Ranges.java

--- String/2559. Count Vowel Strings in Ranges.java
+++ String/2559. Count Vowel Strings in Ranges.java
@@ -1,38 +1,4 @@
 class Solution {
-    public int isVowel(String s){
-        HashSet<Character> set = new HashSet<>(Arrays.asList('a' ,'e' , 'i' , 'o' , 'u'));
-        char l = s.charAt(0);
-        char e = s.charAt(s.length()-1);
-        if(set.contains(l) && set.contains(e)) return 1;
-
-        return 0;
-    }
-    public int[] vowelStrings(String[] words, int[][] queries) {
-        int[] prefix = new int[words.length];
-        prefix[0] = isVowel(words[0]);
-        for(int i=1; i<words.length; i++){
-            prefix[i] = prefix[i-1] + isVowel(words[i]);
-            
-        }
-
-        int m = queries.length;
-        int ans[]= new int[m];
-
-        for(int i=0; i<m; i++){
-            int l = queries[i][0];
-            int e = queries[i][1];
-            ans[i] = (l != 0) ? prefix[e] - prefix[l-1] : prefix[e]; 
-        }
-
-        return ans;
-    
-    }
-}
-
-
-import java.util.*;
-
-class Solution {
     public int isVowel(String s) {
         // Define a set of vowels
         HashSet<Character> set = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
